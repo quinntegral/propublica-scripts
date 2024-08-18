9e87dd4282dd6dc367511f345a64b46d4d9d6969
Jean Quinn <xxj8xg@virginia.edu>
2024-08-18 00:49:43 -0400

    Updated historic script to find latest PDF from filings

--- historic-script.js
+++ historic-script.js
@@ -11,20 +11,43 @@ async function fetchApiData(url) {
     return await response.json();
 }
 
-// helper to get the latest filing data
-function getLatestFilingData(einData) {
-    if (einData.filings_with_data.length > 0 && einData.filings_without_data.length > 0) {
-        let filingWithData = einData.filings_with_data[0];
-        let filingWithoutData = einData.filings_without_data[0];
-        return filingWithData.tax_prd_yr >= filingWithoutData.tax_prd_yr 
-            ? filingWithData 
-            : filingWithoutData;
-    } else if (einData.filings_with_data.length > 0) {
-        return einData.filings_with_data[0]
-    } else if (einData.filings_without_data.length > 0) {
-        return einData.filings_without_data[0]
-    } else return null;
+// helper to 
+function getFilingData(einData) {
+    if (einData.filings_with_data.length <= 0) return null;
+                // add filing data to new record
+            // function for withData, 990, 990PF, or 990EZ
 }
+
+// helper to find most recent filing with a pdf_url, using 2 pointers
+function getLatestPdf(einData) {
+    let { filings_with_data: withData, filings_without_data: withoutData } = einData;
+    let withDataPtr = 0;
+    let withoutDataPtr = 0;
+    let itemWithData, itemWithoutData;
+    while (withDataPtr < withData.length || withoutDataPtr < withoutData.length) {
+        itemWithData = withData[withDataPtr];
+        itemWithoutData = withoutData[withoutDataPtr];
+        // if one list exhausted/empty, return from the other
+        if (withDataPtr >= withData.length)
+            return itemWithoutData.pdf_url ? { 'PDF URL': itemWithoutData.pdf_url, 'PDF Year': itemWithoutData.tax_prd_yr } : null;
+        if (withoutDataPtr >= arrayWithoutData.length)
+            return itemWithData.pdf_url ? { 'PDF URL': itemWithData.pdf_url, 'PDF Year': itemWithData.tax_prd_yr } : null;
+        // if both have valid PDFs, compare years
+        if (itemWithData.pdf_url && itemWithoutData.pdf_url) {
+            return ( itemWithData.tax_prd_yr >= itemWithoutData.tax_prd_yr
+                ? { 'PDF URL': itemWithData.pdf_url, 'PDF Year': itemWithData.tax_prd_yr }
+                : { 'PDF URL': itemWithoutData.pdf_url, 'PDF Year': itemWithoutData.tax_prd_yr }
+            );
+        }
+        // if PDF URL missing, move pointer
+        if (!itemWithData.pdf_url) withDataPtr++;
+        if (!itemWithoutData.pdf_url) withoutDataPtr++;
+    }
+    // no valid PDFs found
+    return null;
+}
+
+
 
 // function to process each record
 async function processFunderRecord(record) {
@@ -37,6 +60,16 @@ async function processFunderRecord(record) {
     if (orgData.total_results == 0) return;
 
     for (let org of orgData.organizations) {
+
+        // query with EIN number
+        let einUrl = `https://projects.propublica.org/nonprofits/api/v2/search.json?q=${org.ein}`;
+        let einData = await fetchApiData(einUrl);
+
+        // synthesize query results
+        filingData = getFilingData(einData.filings_with_data[0]);
+        latestPdf = getLatestPdf(einData);
+
+        // make and add new record
         let newRecord = {
             'Funder': [{ id: record.id }],
             'Name': org.name,
@@ -46,24 +79,11 @@ async function processFunderRecord(record) {
             'State': org.state,
             'NTEE Code': org.ntee
         };
-        
-        let einUrl = `https://projects.propublica.org/nonprofits/api/v2/search.json?q=${org.ein}`;
-        let einData = await fetchApiData(einUrl);
-        let latestFiling = getLatestFilingData(einData);
-        
-        // need to switch based off whether latestFiling is withData or withoutData
-        // AND if withData, if it is 990, 990PF, or 990EZ
-        
-            if (latestFiling) {
-                Object.assign(newRecord, {
-                    'Address': latestFiling.address,
-                    'Filing Year': latestFiling.tax_prd_yr,
-                    '990 Form Link': latestFiling.pdf_url
-                });
-            }
 
-            await nonprofitsTable.createRecordAsync(newRecord);
-        }
+        newRecord = Object.assign(filingData, latestPdf);
+
+        await nonprofitsTable.createRecordAsync(newRecord);
+    }
 }
 
 // main function to run the script
